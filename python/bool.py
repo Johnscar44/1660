d00bf4219d66551d27338bb23cd6f63a3987b6c4
Johnscar44 <50434203+Johnscar44@users.noreply.github.com>
2020-05-09 07:03:03 -0400

    more files added for access

---
+++ python/bool.py
@@ -0,0 +1,23 @@
+#using single and double quotes in a string
+cm_height = "172"
+print("cm_height:",cm_height,"is all digits =",cm_height.isdigit())
+
+#way one of printing bool only works with strings
+string = "python"
+print(string.isalpha())
+
+#way two with out makeing a "python" veriable
+print("python".isalpha()))
+
+
+#boolean string test methods  true or false
+#.isalpha()
+#.isalnum()
+#.istitle()
+#.isdigit()
+#.islower()
+#.isupper()
+#.startswith()
+
+
+print("hello".isupper()
