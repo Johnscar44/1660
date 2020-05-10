e89355d517487be211e5b447ce629314c6847eb8
Johnscar44 <50434203+Johnscar44@users.noreply.github.com>
2020-05-10 02:40:57 -0400

    test finished

---
+++ python/test.py
@@ -0,0 +1,4 @@
+greeting = "Hello"
+print(greeting)
+input_test = input("Enter somethings eaten in the last 24 hours?: ")
+print("It is" , "dairy".lower().upper() in input_test.lower().upper() , input_test , 'contains "dairy"')
