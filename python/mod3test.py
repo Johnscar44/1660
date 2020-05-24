9e8990bd771193c9c4a22a0fa7f9fa56aa855471
Johnscar44 <50434203+Johnscar44@users.noreply.github.com>
2020-05-24 06:30:17 -0400

    mod 3 test done passed

--- python/mod3test.py
+++ python/mod3test.py
@@ -5,15 +5,15 @@
 #Enter cheese order weight (numeric value): 2
 #2.0 costs $15.98
 
-max_weight = "100"
-min_weight = "10"
+max_weight = "101"
+min_weight = "9"
 
 weight = input("how much cheese do you want? max 100/min 10: ")
 if weight.isdigit() == False:
     print("Entry must be a digit")
-elif weight > max_weight:
-    print("That is over the max weight")
-elif weight < min_weight:
-    print("That is not enough")
+elif weight >= max_weight:
+    print(weight , "That is over the maxiumum order amount.")
+elif weight <= min_weight:
+    print( wieght , "That is below minimum order amount.")
 else:
     print(weight , "cost" , float(weight) * 2.05 )
