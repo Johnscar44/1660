09abfaffca75ae1f34bf1724c82d3356ed9aa079
Johnscar44 <50434203+Johnscar44@users.noreply.github.com>
2020-06-09 23:07:07 -0400

    final finished course retired have to do it all over again

--- python/final.py
+++ python/final.py
@@ -1,92 +1,39 @@
-print("")
-print("")
-print("\twelcome to my adding report program!".title())
-print("\tit's been made a little differently.".title())
-print("")
-print("I've made a done command, so you can make mulitible lists\n\t     and you can quit at anytime!")
-print("")
-print("")
-
-def get_report_type():
+def adding_report():
     while True:
-        type = input("what report would you like?\n[A] for all items and total\n[T] for just the total\n[Q] to quit at anytime: ")
-        if do_exit(type):
-            return False
+        type = input("choose report type (\"A\" or \"T\")\nreport types include all items (\"A\") or total only (\"T\")".title())
         if type.isalpha():
             if type.lower() == 'a':
                 return type
             elif type.lower() == 't':
                 return type
-            else:
-                print("incorrect")
-                continue
-
-def total():
-    items = ""
-    total = 0
-    print("T selected")
-    print("enter int!")
-    while True:
-        tot = input("enter int: ")
-        if do_exit(tot):
-            return False
-        if tot.isdigit():
-            items += tot + '\n'
-            total += int(tot)
-            print(tot)
-        elif tot == "Done".lower():
-            print("")
-            print("total:" , total)
-            print("")
-            print("")
-            return False
+            elif type.lower().startswith('q'):
+                return type
         else:
-            ("invalid entry! try again")
-            return None
-
-def add():
-    items = ""
-    total = 0
-    print("A selected")
-    while True:
-        num = input("enter int!: ")
-        if do_exit(num):
-            return False
-        elif num.isdigit():
-                items += num + '\n'
-                total += int(num)
-                print(num)
-        elif num == "Done".lower():
-            print("")
-            print("items\n",items)
-            print("total:" , total)
-            return False
-        else:
-            ("invalid entry! try again")
-            return None
-
-def do_exit(data):
-    if data.isalpha():
-        if data.lower() == 'q':
-            return True
-    else:
-        return False
+            print("incorrect")
 
 
-data = get_report_type()
+
+data = adding_report()
+items = ""
+total = 0
 while True:
-    if data == False:
-        print("goodbye")
-        break
-    elif data == 'a':
-        add()
-    elif data == 't':
-        total()
-    num = add()
-    if num == False:
-        print("gooodbye")
-        break
-    if data == 'a':
-        add()
-    if data == 't':
-        total()
+    x = input("Enter integer or \"Q\" to quit: ")
+    if data == "a":
+        if x.isdigit():
+            items += x + '\n'
+            total += int(x)
+        elif x == "q":
+            print("items\n",items)
+            print("total:",total)
+            break
+        else:
+            print(x , "is invalid input")
+    if data == "t":
+        if x.isdigit():
+            items += x + '\n'
+            total += int(x)
+        elif x == "q":
+            print("total:",total)
+            break
+        else:
+            print(x , "is invalid input")
--- python/list.py
+++ python/list.py
@@ -1,12 +1,26 @@
-items = ""
-total = 0
+def adding_report():
+    items = ""
+    total = 0
+    while True:
+        x = input("Enter integer or \"Q\" to quit: ")
+        if x.isdigit():
+            if x.lower() == 'a':
+                items += x + '\n'
+                total += int(x)
+                print(x)
+            elif x.lower() == 'q':
+                print("items\n",items)
+                print("total:" , total)
+                break
+        elif x.isdigit():
+            if x.lower() == 't':
+                items += x + '\n'
+                total += int(x)
+                print(x)
+            elif x.lower() == 'q':
+                print("total:" , total)
+
+type = adding_report()
 while True:
-    x = input("enter int:")
-    if x:
-        items += x + '\n'
-        total += int(x)
-        print(x)
-    else:
-        print("items\n",items)
-        print("total:" , total)
+    if type == 'a':
         break
