0502250a356c3e112509d11f03c29ac5e1d8e289
Johnscar44 <50434203+Johnscar44@users.noreply.github.com>
2020-07-04 22:05:38 -0400

    new project

--- python/final.py
+++ python/final.py
@@ -6,53 +6,8 @@ def adding_report():
                 return type
             elif type.lower() == 't':
                 return type
-<<<<<<< HEAD
-            else:
-                print("incorrect")
-                continue
-
-def total():
-    items = ""
-    total = 0
-    print("T selected")
-    print("enter int!")
-    while True:
-        tot = input("enter int: ")
-        if do_exit(tot):
-            return False
-            if tot.isdigit():
-                items += tot + '\n'
-                total += int(tot)
-                print(tot)
-            elif tot == "Done".lower():
-                print("")
-                print("total:" , total)
-                print("")
-                print("")
-            else:
-                ("invalid entry! try again")
-                return None
-
-def add():
-    items = ""
-    total = 0
-    print("A selected")
-    while True:
-        num = input("enter int!: ")
-        if do_exit(num):
-            return False
-            if num.isdigit():
-                items += num + '\n'
-                total += int(num)
-                print(num)
-            elif num == "Done".lower():
-                print("")
-                print("items\n",items)
-                print("total:" , total)
-=======
             elif type.lower().startswith('q'):
                 return type
->>>>>>> 6b5f8f75ddcb1479e86942d206328d2db8d8de3e
         else:
             print("incorrect")
 
